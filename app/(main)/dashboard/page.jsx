6ff05dacfcaed9c92d97221217fea5e18f507407
SamriddhoBiswas <samriddhobiswas9@gmail.com>
2025-07-11 21:46:22 +0530

    Added custom hooks and create-account-drawer

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Plus } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import { CreateAccountDrawer } from "@/components/create-account-drawer";
 
 function DashboardPage() {
   return (
-    <div>Page</div>
+    <div>
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add New Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+    </div>
   )
 }
 
